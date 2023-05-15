c6d02f70c0aadb956dc796c0b678557a75e698e5
DouniaBK <douniazedbacha@gmail.com>
2023-05-15 12:33:40 +0000

    JS excercise

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,40 @@
  */
 
 // Vanilla JavaScript Function
+/**  function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);*/
 
 // Arrow Function With Parameters
+/**const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum); */
 
 // Single Line Arrow Function With Parameters
+/**
+
+const addTwoNumbers4 = (a, b) => a + b;
+let sum2 = addTwoNumbers4(6, 4);
+console.log(sum2);*/
 
 // Implicit Returns
+/** 
+const saySomething = message => console.log(message);
+saySomething('Hello there');
+const sayHello = () => console.log('hello');
+sayHello();*/
+
 
 // Returning Multiple Lines
+
+const retrunMultipleLines = () => (
+    '<p> dont be sacred of javascript! you got it </p>'
+)
+
+console.log(retrunMultipleLines());
